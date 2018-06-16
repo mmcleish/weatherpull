7ea0ba0c6c078753492667e89e73a8f3444786f6
mmcleish <michael.b.mcleish@gmail.com>
2018-06-16 13:38:04 -0400

    new code

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # weatherpull
+# Test 
--- weatherpull.py
+++ weatherpull.py
@@ -1,1 +1,22 @@
+#!/usr/bin/python3
 #v1
+#import lib
+import urllib2
+#import bs4 from 
+from bs4 import BeautifulSoup
+
+
+page = 
+requests.get('https://weather.gc.ca/city/pages/on-25_metric_e.html', 
+verify=False)
+soup = BeautifulSoup(page.content, html.parser')
+print(soup.title)
+
+
+#url scrape
+#weather = "https://weather.gc.ca/city/pages/on-25_metric_e.html"
+#query
+#page = urllib2.urlopen(weater)
+#parse
+#soup = BeautifulSoup(page, `html.parser`)
+#print soup
